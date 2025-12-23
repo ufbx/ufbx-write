b31977f603dacd411e19008db80086f793cdf31c
Samuli <bqqbarbhg@gmail.com>
2025-12-23 15:03:56 +0900

    Embedded content tests

--- test/test/test_texture.h
+++ test/test/test_texture.h
@@ -51,3 +51,75 @@ UFBXWT_SCENE_CHECK(video_content)
 }
 #endif
 
+UFBXWT_SCENE_TEST(video_large_content)
+#if UFBXWT_IMPL
+{
+	ufbxw_video video = ufbxw_create_video(scene);
+	ufbxw_set_name(scene, video.id, "EmbeddedVideo");
+
+	const size_t content_size = 8 * 1024;
+	ufbxw_byte_buffer buffer = ufbxw_create_byte_buffer(scene, content_size);
+	ufbxw_byte_list list = ufbxw_edit_byte_buffer(scene, buffer);
+	for (size_t i = 0; i < content_size; i++) {
+		list.data[i] = (char)((i * 0x9e3779b9u) >> 24);
+	}
+
+	ufbxw_video_set_content(scene, video, buffer);
+}
+#endif
+
+UFBXWT_SCENE_CHECK(video_large_content)
+#if UFBXWT_IMPL
+{
+	ufbx_video *video = (ufbx_video*)ufbx_find_element(scene, UFBX_ELEMENT_VIDEO, "EmbeddedVideo");
+	ufbxwt_assert(video);
+
+	const size_t content_size = 8 * 1024;
+	ufbxwt_assert(video->content.size == content_size);
+
+	const char *content = video->content.data;
+	for (size_t i = 0; i < content_size; i++) {
+		ufbxwt_assert(content[i] == (char)((i * 0x9e3779b9) >> 24));
+	}
+}
+#endif
+
+#if UFBXWT_IMPL
+static size_t ufbxwt_video_content_stream(void *user, void *dst, size_t dst_size, size_t offset)
+{
+	char *d = (char*)dst;
+	for (size_t i = 0; i < dst_size; i++) {
+		d[i] = (char)(((offset + i) * 0x9e3779b9u) >> 24);
+	}
+	return dst_size;
+}
+#endif
+
+UFBXWT_SCENE_TEST(video_stream_content)
+#if UFBXWT_IMPL
+{
+	ufbxw_video video = ufbxw_create_video(scene);
+	ufbxw_set_name(scene, video.id, "EmbeddedVideo");
+
+	const size_t content_size = 8 * 1024;
+	ufbxw_byte_buffer buffer = ufbxw_external_byte_stream(scene, &ufbxwt_video_content_stream, NULL, content_size);
+
+	ufbxw_video_set_content(scene, video, buffer);
+}
+#endif
+
+UFBXWT_SCENE_CHECK(video_stream_content)
+#if UFBXWT_IMPL
+{
+	ufbx_video *video = (ufbx_video*)ufbx_find_element(scene, UFBX_ELEMENT_VIDEO, "EmbeddedVideo");
+	ufbxwt_assert(video);
+
+	const size_t content_size = 8 * 1024;
+	ufbxwt_assert(video->content.size == content_size);
+
+	const char *content = video->content.data;
+	for (size_t i = 0; i < content_size; i++) {
+		ufbxwt_assert(content[i] == (char)((i * 0x9e3779b9) >> 24));
+	}
+}
+#endif
